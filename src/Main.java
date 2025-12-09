c294f8a595964d8b0bdf66ab253cddf90e66a34d
bogomolova <ibogomolllova@gmail.com>
2025-12-09 17:32:42 +0200

    isPalindrome

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,32 @@
-import java.util.Arrays;
-
 public class Main {
 
     /**
-     * Задача: Move Zeroes (переместить все нули в конец массива)
-     * -------------------------------------------------------------
-     * Дано: целочисленный массив nums, содержащий нули и не-нули. :contentReference[oaicite:0]{index=0}
-     * Требуется: модифицировать массив на месте — так, чтобы:
-     * 1) все нули оказались в конце массива;
-     * 2) порядок ненулевых элементов (относительно друг друга) остался тем же, что и в исходном массиве.
-     * <br>
-     * Вход / Выход — примеры:
-     * Input: nums = [0, 1, 0, 3, 12]
-     * После вызова moveZeroes(nums), nums должен стать [1, 3, 12, 0, 0]
-     * <br>
-     * Input: nums = [0]
-     * После вызова — nums остаётся [0]
-     * <br>
-     * Замечание: метод ничего не возвращает — модификация происходит in-place (в самом массиве)
+     * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+     * and removing all non-alphanumeric characters, it reads the same forward and backward.
+     * Alphanumeric characters include letters and numbers.
+     * Given a string s, return true if it is a palindrome, or false otherwise.
      */
     public static void main(String[] args) {
-        int[] nums = {0, 0, 1, 0, 3, 0, 9, 2};
-        moveZeroes(nums);
+        String str1 = "A man, a plan, a canal: Panama";
+        String str2 = "race a car";
+        String str3 = " ";
+
+        boolean result = isPalindrome(str2);
+        System.out.println(result);
     }
 
-    public static void moveZeroes(int[] nums) {
-        int posNoZero = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                if (i != posNoZero) {
-                    int timeVar = nums[i];
-                    nums[i] = nums[posNoZero];
-                    nums[posNoZero] = timeVar;
-                }
-                posNoZero++;
+    public static boolean isPalindrome(String str) {
+        if (str != null ) {
+
+            String mod1 = str.replaceAll("[^A-Za-zА-Яа-я]", "");
+            String mod2 = mod1.toLowerCase();
+            String reverseStr = new StringBuilder(mod2).reverse().toString();
+
+            if (reverseStr.equals(mod2)) {
+                return true;
             }
         }
-        System.out.println(Arrays.toString(nums));
+        return false;
     }
+
 }
